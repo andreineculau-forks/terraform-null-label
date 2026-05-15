39f3363acb619fd0b51a9e54f645b664c327f5a8
Andrei Neculau <andrei.neculau@gmail.com>
2026-05-15 14:18:18 +0200

    refactor for readability
    Signed-off-by: Andrei Neculau <andrei.neculau@gmail.com>

--- main.style.tf
+++ main.style.tf
@@ -2,76 +2,93 @@ locals {
   # tflint-ignore: terraform_naming_convention
   _style = coalesce(var.style, local._defaults.style)
   # tflint-ignore: terraform_naming_convention
+  _style_family = (
+    local._style == "aws" || startswith(local._style, "aws_") ? "aws" :
+    local._style == "confluent" || startswith(local._style, "confluent_") ? "confluent" :
+    local._style == "google" || startswith(local._style, "google_") ? "google" :
+    local._style == "snowflake" || startswith(local._style, "snowflake_") ? "snowflake" :
+    "general"
+  )
+  # tflint-ignore: terraform_naming_convention
   _delimiter = (
     # Google exceptions
     local._style == "google_iam_role" ? "_" :
     # Snowflake exceptions
-    local._style == "snowflake" || startswith(local._style, "snowflake_") ? coalesce(var.delimiter, "_") :
-    # Fallback (default)
+    local._style_family == "snowflake" ? coalesce(var.delimiter, "_") :
+    # General
     coalesce(var.delimiter, "-")
   )
+
   # tflint-ignore: terraform_naming_convention
-  _regex_replace_chars = (
-    # Confluent exceptions
-    local._style == "confluent_connector" ? "/[^${local._delimiter}a-zA-Z0-9.,&_+|[]]/" :
-    local._style == "confluent_topic" ? "/[^${local._delimiter}a-zA-Z0-9_]/" :
-    # Google exceptions
-    local._style == "google_iam_role" ? "/[^${local._delimiter}a-zA-Z0-9.]/" :
-    # Fallback (default)
-    "/[^${local._delimiter}a-zA-Z0-9]/"
-  )
+  _style_family_overrides = {
+    google = {
+      label_key_case = "lower"
+    }
+    snowflake = {
+      delimiter        = "_"
+      id_length_limit  = 255
+      label_value_case = "upper"
+    }
+  }
+
+  # tflint-ignore: terraform_naming_convention
+  _style_overrides = {
+    aws_s3_bucket = {
+      id_length_limit = 255
+    }
+    confluent_connector = {
+      id_length_limit     = 64
+      regex_replace_chars = "/[^${local._delimiter}a-zA-Z0-9.,&_+|[]]/"
+    }
+    confluent_topic = {
+      id_length_limit     = 255
+      regex_replace_chars = "/[^${local._delimiter}a-zA-Z0-9_]/"
+    }
+    google_iam_role = {
+      delimiter           = "_"
+      regex_replace_chars = "/[^${local._delimiter}a-zA-Z0-9.]/"
+      id_length_limit     = 64
+      id_hash_unique      = true
+    }
+    google_iam_service_account = {
+      id_length_limit = 30
+    }
+    google_storage_bucket = {
+      id_length_limit = 63
+    }
+    google_storage_bucket_dns = {
+      id_length_limit = 255
+    }
+    snowflake_database = {
+      label_order = ["namespace", "tenant", "environment"]
+    }
+    snowflake_database_object = {
+      label_order = ["name", "attributes"]
+    }
+    snowflake_schema = {
+      label_order = ["stage"]
+    }
+    snowflake_schema_object = {
+      label_order = ["name", "attributes"]
+    }
+    snowflake_space = {
+      label_order = ["namespace", "tenant"]
+    }
+  }
 
   defaults = merge(
     local._defaults,
     {
-      label_order = (
-        # Snowflake exceptions
-        local._style == "snowflake_database" ? ["namespace", "tenant", "environment"] :
-        local._style == "snowflake_database_object" ? ["name", "attributes"] :
-        local._style == "snowflake_schema" ? ["stage"] :
-        local._style == "snowflake_schema_object" ? ["name", "attributes"] :
-        local._style == "snowflake_space" ? ["namespace", "tenant"] :
-        # Fallback (default plus tenant)
-        ["namespace", "tenant", "environment", "stage", "name", "attributes"]
-      )
+      label_order         = ["namespace", "tenant", "environment", "stage", "name", "attributes"]
       delimiter           = local._delimiter
-      regex_replace_chars = local._regex_replace_chars
-      id_length_limit = (
-        # AWS exceptions
-        local._style == "aws_s3_bucket" ? 255 :
-        # Confluent exceptions
-        local._style == "confluent_connector" ? 64 :
-        local._style == "confluent_topic" ? 255 :
-        # Google exceptions
-        local._style == "google_storage_bucket" ? 63 :
-        local._style == "google_iam_role" ? 64 :
-        local._style == "google_iam_service_account" ? 30 :
-        local._style == "google_storage_bucket_dns" ? 255 :
-        # Snowflake
-        local._style == "snowflake" || startswith(local._style, "snowflake_") ? 255 :
-        # Fallback
-        63
-      )
-      id_hash_length = 12 # mimic CloudFormation's hash length
-      id_hash_unique = (
-        # Google exceptions
-        # see https://github.com/hashicorp/terraform-provider-google/issues/11311
-        local._style == "google_iam_role" ? true :
-        # Fallback (default)
-        false
-      )
-      label_key_case = (
-        # Google
-        local._style == "google" || startswith(local._style, "google_") ? "lower" :
-        # Fallback (default)
-        "title"
-      )
-      label_value_case = (
-        # Snowflake
-        local._style == "snowflake" || startswith(local._style, "snowflake_") ? "upper" :
-        # Fallback (default)
-        "lower"
-      )
-    }
+      regex_replace_chars = "/[^${local._delimiter}a-zA-Z0-9]/"
+      id_length_limit     = 63
+      id_hash_length      = 12 # mimic CloudFormation's hash length
+      id_hash_unique      = false
+      label_key_case      = "title"
+      label_value_case    = "lower"
+    },
+    lookup(local._style_family_overrides, local._style_family, {}),
+    lookup(local._style_overrides, local._style, {})
   )
 }
